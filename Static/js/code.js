24df7dd2af0c8b2f527e6d4a9239bdf429e13f55
MiltonVilchez <vilchezmilton8@gmail.com>
2022-05-07 00:33:31 -0600

    Casi listo el maquetado del cuenta.html

---
+++ Static/js/code.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded",() => {
+//     if(localStorage.username == undefined){
+//         document.getElementById("DivLogIn").classList.add("divOcultar")
+//     }else{
+
+//     }
+// });
