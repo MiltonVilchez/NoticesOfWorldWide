bb34901dc7affc4dcc736068bfd38ca85ab46e34
MiltonVilchez <vilchezmilton8@gmail.com>
2022-05-08 22:52:28 -0600

    Añadi a la pagina cuenta.html

--- Static/js/code.js
+++ Static/js/code.js
@@ -1,7 +1,13 @@
-// document.addEventListener("DOMContentLoaded",() => {
-//     if(localStorage.username == undefined){
-//         document.getElementById("DivLogIn").classList.add("divOcultar")
-//     }else{
-
-//     }
-// });
+document.addEventListener("DOMContentLoaded",() => {
+    if(localStorage.username == undefined){
+        document.getElementById("DivLogIn").classList.remove("divOcultar");
+    }else{
+        document.getElementById("DivRegister").classList.add("divOcultar");
+        document.getElementById("DivLogIn").classList.add("divOcultar");
+        document.getElementById("cuenta").classList.remove("divOcultar");
+    }
+    registrar = () => {
+        document.getElementById("DivRegister").classList.remove("divOcultar");
+        document.getElementById("DivLogIn").classList.add("divOcultar");
+    }
+});
