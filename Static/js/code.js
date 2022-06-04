9dee259170fe3261d15245168ddc4ef2fce67fd0
MiltonVilchez <vilchezmilton8@gmail.com>
2022-06-04 13:31:43 -0600

    Listo para revision

--- Static/js/code.js
+++ Static/js/code.js
@@ -1,13 +1,30 @@
-document.addEventListener("DOMContentLoaded",() => {
-    if(localStorage.username == undefined){
-        document.getElementById("DivLogIn").classList.remove("divOcultar");
-    }else{
-        document.getElementById("DivRegister").classList.add("divOcultar");
-        document.getElementById("DivLogIn").classList.add("divOcultar");
-        document.getElementById("cuenta").classList.remove("divOcultar");
-    }
-    registrar = () => {
-        document.getElementById("DivRegister").classList.remove("divOcultar");
-        document.getElementById("DivLogIn").classList.add("divOcultar");
-    }
+const exampleModal = document.getElementById("exampleModal");
+exampleModal.addEventListener("show.bs.modal", (event) => {
+  // Button that triggered the modal
+  const button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute("data-bs-whatever");
+  const recipientPhoto = button.getAttribute("data-foto");
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector(".modal-title");
+
+  modalTitle.textContent = recipient;
+  document.getElementById("rutaImagen").src=recipientPhoto;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.username == undefined) {
+    document.getElementById("DivLogIn").classList.remove("divOcultar");
+  } else {
+    document.getElementById("DivRegister").classList.add("divOcultar");
+    document.getElementById("DivLogIn").classList.add("divOcultar");
+    document.getElementById("cuenta").classList.remove("divOcultar");
+  }
+  registrar = () => {
+    document.getElementById("DivRegister").classList.remove("divOcultar");
+    document.getElementById("DivLogIn").classList.add("divOcultar");
+  };
 });
