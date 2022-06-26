a4571aeffbc84602aac2df141264b55623375b2d
MiltonVilchez <vilchezmilton8@gmail.com>
2022-06-26 12:18:30 -0600

    Viendo si funciona la geolocalizacion en el servidor

---
+++ Static/js/geolocal.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+    const info = document.querySelector("#infolocated");
+
+    mapLink.href = '';
+    mapLink.textContent = '';
+
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        status.textContent = '';
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = 'Tú ubicación actual';
+        info.innerHTML = 'Tú ubicación la ocuparemos para mostrar noticias de tú país';
+    }
+
+    function error() {
+        status.textContent = 'No se aceptaron los permisos';
+    }
+
+    if (!navigator.geolocation) {
+        status.textContent = 'La Geolocalización no esta disponible en tu navegador';
+    } else {
+        status.textContent = 'Localizando...';
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+})
